3c491d4c0f750cf109978319355679f5c8d377e0
Biebem <Biebem@DESKTOP-VSDU12I>
2020-10-25 13:10:10 +0100

    fixed viewcomicmapper

--- ViewModel/Mapper.cs
+++ ViewModel/Mapper.cs
@@ -15,7 +15,7 @@ namespace ViewModel
         /// <returns></returns>
         public static Comic ViewComicMapper(ViewComic viewcomic) 
         {
-            return new Comic(viewcomic.Title, viewcomic.Series, viewcomic.SeriesNumber, ViewAuthorMapper(viewcomic.Authors), viewcomic.Publisher);
+            return new Comic(viewcomic.Title, viewcomic.Series, viewcomic.SeriesNumber, ViewAuthorMapper(viewcomic.Authors), new Publisher(viewcomic.Publisher.Name));
         }
         /// <summary>
         /// Maps a list of ViewAuthors objects to Authors objects. So that it can be transfered to the domainLayer.
