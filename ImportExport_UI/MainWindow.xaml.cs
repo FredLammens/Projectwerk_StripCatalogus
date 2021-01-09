010ce8d22756edd4ae179f88eb5ba944e03ff80c
Biebem <lammens.f96@gmail.com>
2021-01-09 21:35:37 +0100

    Added documentation

--- ImportExport_UI/MainWindow.xaml.cs
+++ ImportExport_UI/MainWindow.xaml.cs
@@ -10,16 +10,28 @@ namespace ImportExport_UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// viewmodel for import/Export.
+        /// </summary>
         private ImportExportViewModel vm = new ImportExportViewModel();
+        /// <summary>
+        /// constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = vm;
         }
+        /// <summary>
+        /// Import comics on btn click.
+        /// </summary>
         private async void ImportComicsBtn_Click(object sender, RoutedEventArgs e)
         {
             await Task.Run(() => Importcomics());
         }
+        /// <summary>
+        /// asynchronously imports comic after showing dialog.
+        /// </summary>
         private void Importcomics()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -28,17 +40,17 @@ namespace ImportExport_UI
                 path = openFileDialog.FileName;
             try
             {
-                if (vm.Import(path) == true)
-                    MessageBox.Show("finished importing");
+                vm.Import(path);
+                MessageBox.Show("klaar met importeren.");
             }
             catch (Exception ex)
             {
-                if (MessageBox.Show(ex.Message, "Error parsing file", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                if (MessageBox.Show(ex.Message, "fout bij inlezen bestand", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
                 {
                     try
                     {
-                        if (vm.ContinueImport() == true)
-                            MessageBox.Show("finished importing");
+                        vm.ContinueImport();
+                        MessageBox.Show("klaar met importeren.");
                     }
                     catch (Exception exception)
                     {
@@ -49,18 +61,26 @@ namespace ImportExport_UI
                     vm.ResetPanels();
             }
         }
-        private void export(string path) 
+        /// <summary>
+        /// Exports comics to path
+        /// </summary>
+        /// <param name="path">path to export to</param>
+        private void export(string path)
         {
 
             try
             {
                 vm.Export(path);
+                MessageBox.Show("klaar met exporteren.");
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
         }
+        /// <summary>
+        /// asynchronously exports comics.
+        /// </summary>
         private async void btnExport_Click(object sender, RoutedEventArgs e)
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
--- ViewModel/ImportExportViewModel.cs
+++ ViewModel/ImportExportViewModel.cs
@@ -10,17 +10,33 @@ namespace ViewModel
     public class ImportExportViewModel : ViewModelBase
     {
         #region private members
+        /// <summary>
+        /// DomainController
+        /// </summary>
         private readonly Controller controller;
+        /// <summary>
+        /// holds deserialized Comics
+        /// </summary>
         private List<ViewComic> comics;
+        /// <summary>
+        /// holds a list of double viewcomics
+        /// </summary>
         private List<ViewComic> doubles;
+        /// <summary>
+        /// Mapped comics
+        /// </summary>
         private List<Comic> comicsToImport;
+
         private double _percentage;
         private bool _showLoadingPanel = false;
         private bool _showButtonPanel = true;
-        private bool _showLoadingBar = true; //Todo:
+        private bool _showLoadingBar = true;
         private string _loadingText;
         #endregion
         #region Properties
+        /// <summary>
+        /// Databinded percentage for loadingbar
+        /// </summary>
         public double Percentage
         {
             get { return _percentage; }
@@ -30,6 +46,9 @@ namespace ViewModel
                 OnPropertyChanged("Percentage");
             }
         }
+        /// <summary>
+        /// Databinded bool for visibility of loadingPanel
+        /// </summary>
         public bool ShowLoadingPanel
         {
             get { return _showLoadingPanel; }
@@ -39,6 +58,9 @@ namespace ViewModel
                 OnPropertyChanged("ShowLoadingPanel");
             }
         }
+        /// <summary>
+        /// Databinded bool for visibility of buttonPanel
+        /// </summary>
         public bool ShowButtonPanel
         {
             get { return _showButtonPanel; }
@@ -48,6 +70,9 @@ namespace ViewModel
                 OnPropertyChanged("ShowButtonPanel");
             }
         }
+        /// <summary>
+        /// Databinded text for loadingPanel
+        /// </summary>
         public string LoadingText 
         {
             get => _loadingText;
@@ -57,6 +82,9 @@ namespace ViewModel
                 OnPropertyChanged();
             }
         }
+        /// <summary>
+        /// Databinded bool for visibility of loadingPanel loadingbar
+        /// </summary>
         public bool ShowLoadingBar 
         {
             get => _showLoadingBar;
@@ -67,12 +95,21 @@ namespace ViewModel
             }
         }
         #endregion
+        #region Constructor
+        /// <summary>
+        /// Controller that adds the domainController
+        /// </summary>
         public ImportExportViewModel()
         {
             controller = new Controller(new UnitOfWork());
         }
-
-        public bool Import(string comicsFilePath)
+        #endregion
+        #region Methods
+        /// <summary>
+        /// imports comics from file.
+        /// </summary>
+        /// <param name="comicsFilePath"></param>
+        public void Import(string comicsFilePath)
         {
             LoadingText = "Checken op dubbele strips.";
             Percentage = 0;
@@ -86,15 +123,20 @@ namespace ViewModel
                                 .ToList();
             if (doubles.Count > 0)
                 throw new PresentationException($"Er zijn {doubles.Count} dubbele strips gevonden. wilt u dit overslaan?");
-            return ContinueImport();
+            ContinueImport();
         }
+        /// <summary>
+        /// Resetpannels to show buttonpanel only
+        /// </summary>
         public void ResetPanels() 
         {
             ShowLoadingPanel = false;
             ShowButtonPanel = true;
         }
-
-        public bool ContinueImport()
+        /// <summary>
+        /// continues import after removing any doubles
+        /// </summary>
+        public void ContinueImport()
         {
             if (doubles.Count > 0)
             {
@@ -116,9 +158,11 @@ namespace ViewModel
             controller.AddComics(comicsToImport);
             ShowLoadingPanel = false;
             ShowButtonPanel = true; 
-            return true;
         }
-
+        /// <summary>
+        /// Exports comics from catalogue to exportpath
+        /// </summary>
+        /// <param name="exportPath">path to export.</param>
         public void Export(string exportPath)
         {
             ShowLoadingBar = false;
@@ -130,5 +174,6 @@ namespace ViewModel
             ShowButtonPanel = true;
             ShowLoadingBar = true;
         }
+        #endregion
     }
 }
